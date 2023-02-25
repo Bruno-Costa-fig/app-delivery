3eed1b1c6e41731b69a0d5c6a85bd8a6707a89ed
Bruno Costa <brunocf.dev@hotmail.com>
2023-02-25 15:36:05 -0300

    base do projeto

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <nav>
+      <router-link class="nav-item" to="/">Home</router-link>
+      <router-link class="nav-item" to="/novo-pedido">Novo pedido</router-link>
+      <router-link class="nav-item" to="/novo-produto">Novo produto</router-link>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+  nav {
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+  }
+
+  .nav-item {
+    padding: 15px 20px;
+    text-decoration: none;
+    color: #06b306;
+    background: #eafcea;
+    margin-left: 20px;
+    border-radius: 15px;
+  }
+
+  .nav-item:hover {
+    color: #eafcea;
+    background: #75f175;
+  }
+
+</style>
